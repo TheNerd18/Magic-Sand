1f0e1430c79bba46fc4eb10e7638fc81aea2f670
Matthew Jay <49015943+TheNerd18@users.noreply.github.com>
2022-10-14 17:18:51 +1300

    removed unnecessary code

--- src/CustomImageCreator/createCustom.py
+++ src/CustomImageCreator/createCustom.py
@@ -2,12 +2,9 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
 import sys
 import os
 
-#had to change some restrictions sudo mv /etc/ImageMagick-6/policy.xml /etc/ImageMagick-6/policy.xmlout removed restrictions
-# call sudo mv /etc/ImageMagick-6/policy.xmlout /etc/ImageMagick-6/policy.xml to put policy backconvert image.pdf image_%0d.tiff
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.convert_file()
 
     def convert_file(self):
@@ -21,10 +18,6 @@ class MainWindow(QMainWindow):
             initialFilter= file_filter
         )
 
-        print(response[0])
-        ppt_pdf = 'soffice --headless --convert-to pdf '+response[0]
-        os.system(ppt_pdf)
-        
         pdf_tif = 'convert -density 100 '+response[0]+' bin/data/layerImages/custom/100_%02d.tif'
         os.system(pdf_tif)
 
